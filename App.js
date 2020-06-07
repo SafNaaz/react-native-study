7e0ab33b1a40423d581b0a485436d4fdd784dfcb
SAFNAS <iamsafnas@gmail.com>
2020-06-07 13:03:38 +0530

    rendered a flatlist

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/header';
+import uuid from 'uuid-random';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
-    <View style={styles.containor}>
+    <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containor: {flex: 1},
+  container: {flex: 1},
 });
 
 export default App;
