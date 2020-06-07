bcc165d89de04efb03a03faf9497aa6ab09294bd
SAFNAS <iamsafnas@gmail.com>
2020-06-07 11:22:17 +0530

    added a random image

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.containor}>
       <Text style={styles.text}>Hello World!</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+        style={styles.img}
+      />
     </View>
   );
 };
@@ -12,6 +16,11 @@ const App = () => {
 const styles = StyleSheet.create({
   containor: {flex: 1, justifyContent: 'center', alignItems: 'center'},
   text: {color: 'darkslateblue', fontSize: 20},
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
 });
 
 export default App;
